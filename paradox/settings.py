b085b7e2624d280746c49f6e088dd311227ddf85
nitingera1996 <nitin.gera.ece14@iitbhu.ac.in>
2016-01-08 20:39:20 +0530

    still fixing

--- paradox/settings.py
+++ paradox/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATIC_PATH = os.path.join(BASE_DIR,'static').replace('\\','/')
 STATICFILES_DIRS = (
