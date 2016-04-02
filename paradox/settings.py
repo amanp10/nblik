eaa55507fe902d2b1f0495e3aee74ddea5f6a202
amanp10 <amanpratik10@gmail.com>
2016-04-02 11:29:08 +0530

    Apr 2

--- paradox/settings.py
+++ paradox/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = (
     'ckeditor',
     'ckeditor_uploader',
     'imagekit',
+    'storages',
 )
 
 AUTHENTICATION_BACKENDS = (
@@ -130,13 +131,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 STATIC_PATH = os.path.join(BASE_DIR,'static').replace('\\','/')
 STATICFILES_DIRS = (
     STATIC_PATH,
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-MEDIA_URL = '/media/'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(STATIC_PATH,'media')
 #EMAIL_HOST = 'smtp.gmail.com'
 #EMAIL_USE_TLS = True
@@ -188,3 +188,22 @@ CKEDITOR_ALLOW_NONIMAGE_FILES = False
 #         'toolbar': 'Basic',
 #     },
 # }
+AWS_STORAGE_BUCKET_NAME = 'elasticbeanstalk-us-west-2-031348677605'
+AWS_ACCESS_KEY_ID = 'AKIAIGK3MBDZNNUI7OAQ'
+AWS_SECRET_ACCESS_KEY = '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'
+
+    # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+    # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+    # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+    # We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+    # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+    # refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+    # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+    # you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
