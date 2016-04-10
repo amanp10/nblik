f84ef6292873e41e817a200ba5800d26eefe71ae
amanp10 <amanpratik10@gmail.com>
2016-04-11 02:53:04 +0530

    Apr 11 3

--- paradox/settings.py
+++ paradox/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = 'staticfiles'
 #STATIC_URL = '/static/'
-STATIC_PATH = os.path.join(BASE_DIR,'static').replace('\\','/')
+STATIC_PATH = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = (
     STATIC_PATH,
 )
